fbb79cd05159511106b02c52499fefce01cff225
Christophe Jacquet <ChristopheJacquet@users.noreply.github.com>
2011-09-12 21:22:31 +0000

    GPL license
    git-svn-id: https://speedy.supelec.fr/SVNExt/svn/Envoi/trunk@6 d8d9ab62-b78d-48c5-8992-ee621cfb12a8

--- inc/code.php
+++ inc/code.php
@@ -1,5 +1,26 @@
 <?php
 
+
+/*
+    This file is part of Envoi.
+
+    Envoi is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Envoi is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Envoi.  If not, see <http://www.gnu.org/licenses/>.
+
+    (c) Christophe Jacquet, 2009-2011.
+ */
+
+
 function genereCode($id) {
     // on utilise un code linéaire (26, 10) : l'ID est sur 16 bits, et on ajoute 10 bits de contrôle
     // on prend comme polynôme générateur x10 + x8 + x7 + x5 + x4 + x3 + 1
