d7b762c81f64cce5d28904dd766b3b5727283973
Christophe Jacquet <ChristopheJacquet@users.noreply.github.com>
2011-09-20 20:11:38 +0000

    Fichiers en UTF-8
    git-svn-id: https://speedy.supelec.fr/SVNExt/svn/Envoi/trunk@9 d8d9ab62-b78d-48c5-8992-ee621cfb12a8

--- public/zipfichier.php
+++ public/zipfichier.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <title>Envoi &ndash; Visualisation de fichier</title>
         <script type="text/javascript" src="syntaxhighlighter/scripts/shCore.js"></script>
 	<script type="text/javascript" src="syntaxhighlighter/scripts/shBrushJava.js"></script>
